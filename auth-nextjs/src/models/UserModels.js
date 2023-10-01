9e543db8e923e6497fcd4ac31b0145da8f17f9f7
Sumanjeet <sumanjeet0012@gmail.com>
2023-10-01 04:47:37 +0000

    created user model & schema

---
+++ auth-nextjs/src/models/UserModels.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: {
+        type: String,
+    },
+    forgotPasswordTokenExpiry: {
+        type: Date,
+    },
+    verifyToken: {
+        type: String,
+    },
+    verifyTokenExpiry: {
+        type: Date,
+    },
+    
+});
+
+const User = mongoose.models.User || mongoose.model('User', UserSchema);
+
+export default User;
