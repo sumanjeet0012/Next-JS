8f061ec6e1e0173ba504dd626e2c38f220be96cc
sumanjeet0012 <sumanjeet0012@gmail.com>
2023-10-22 12:49:17 +0530

    fixed the verify token expiry

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -15,8 +15,7 @@ export async function POST(request: NextRequest){
         const {token} = reqBody
         console.log(token);
 
-        // const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
-        const user = await User.findOne({verifyToken: token});
+        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
         console.log("hhhhhhhhhii");
         console.log(user);
         // console.log(t);
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -10,7 +10,7 @@ export const sendEmail = async({email, emailType, userId}: any) => {
         if(emailType === "VERIFY"){
         await User.findByIdAndUpdate(userId, 
             {verifyToken: hashedToken,
-             verifyTokenExpires: Date.now() + 3600000})}
+            verifyTokenExpiry: Date.now() + 3600000})}
         
         else if(emailType === "RESET"){
             
